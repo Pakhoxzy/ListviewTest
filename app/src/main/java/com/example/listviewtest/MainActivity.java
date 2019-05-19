9116e75723a680a0a5256cab79b62e94600efc71
Pakho <270168373@qq.com>
2019-05-19 15:11:13 +0800

    1.3 broadcast receiver

---
+++ app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.listviewtest;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+    private MyBcReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
+    private IntentFilter intentFilter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        //初始化广播接收者，设置过滤器
+        localReceiver = new MyBcReceiver();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.jay.mybcreceiver.LOGIN_OTHER");
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.jay.mybcreceiver.LOGIN_OTHER");
+                localBroadcastManager.sendBroadcast(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+}
